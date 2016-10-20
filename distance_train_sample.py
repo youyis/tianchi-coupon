5fa4a3022b08fc1e2da8f052e1ccaedafe0a064c
nieyanping <nieyanping@jd.com>
2016-10-20 19:08:49 +0800

    label

---
+++ distance_train_sample.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+import numpy as np
+
+os.chdir("/data0/syy/coupon/tianchi-coupon")
+os.listdir(os.getcwd())
+offline = pd.read_csv('../data/base_sample.csv')
+
+distance_pos = offline['User_id'].groupby([offline['distance'],offline['label']]).count().reset_index()
+distance = []
+
+
+for i in xrange(offline.shape[0]):ii
+    dist = offline['distance'][i]
+    pos = distance_pos['User_id'][(distance_pos['distance'] == dist) & (distance_pos['label'] == 1)]
+    all = distance_pos['User_id'][(distance_pos['distance'] == dist)].sum()
+    distance.append(pos / all)
+offline['distance'] = distance
+
+offline.to_csv('../data/distance_sample.csv',index=False)
--- xgb.py
+++ xgb.py
@@ -20,7 +20,7 @@ def get_params():
 
 
 # train sample
-train_sample =  pd.read_csv('../data/label_id.csv')
+train_sample =  pd.read_csv('../data/label_sample.csv')
 drop_col = ['User_id','Merchant_id','Coupon_id','Date_received']
 sample_drop = train_sample.drop(drop_col,axis=1)
 feature,label = sample_drop.iloc[:,1:],sample_drop.iloc[:,0]
@@ -32,10 +32,11 @@ test_sample =  pd.read_csv('../data/test_sample.csv')
 test_feat = test_sample.drop(drop_col,axis=1)
 
 
-
+# xgboost sample
 xgtrain = xgb.DMatrix(train_feat, train_label)
 xgvalid = xgb.DMatrix(valid_feat, valid_label)
 xgtest =  xgb.DMatrix(test_feat)
+xgtrain_all = xgb.DMatrix(feature, label)
 
 
 # xgboost parameter
@@ -43,8 +44,10 @@ plst = get_params()
 plst += [('eval_metric', 'auc')]
 print(plst)
 watchlist = [(xgvalid, 'valid'),(xgtrain,'train')]
-xgb_num_rounds = 70
+xgb_num_rounds = 100
 model = xgb.train(params = plst, dtrain = xgtrain, evals = watchlist,num_boost_round = xgb_num_rounds)
+
+model_all = xgb.train(params = plst, dtrain = xgtrain_all, evals = watchlist,num_boost_round = xgb_num_rounds)
 
 # feature importance 
 importance = model.get_fscore()
@@ -57,14 +60,16 @@ for k,v in tuples:
 #xgb.plot_importance(model)
 valid_preds = model.predict(xgvalid, ntree_limit=model.best_iteration)
 test_preds = model.predict(xgtest, ntree_limit=model.best_iteration)
-
+test_preds_all = model_all.predict(xgtest, ntree_limit=model.best_iteration)
 
 preds_out = pd.DataFrame()
 preds_out['User_id'] = test_sample['User_id']
 preds_out['Merchant_id']  =  test_sample['Merchant_id'] 
 preds_out['Coupon_id'] =  test_sample['Coupon_id']
 preds_out['Date_received'] =  test_sample['Date_received']
-preds_out['preds'] = test_preds
+preds_out['preds'] = test_preds_all
+
+
 
 preds_res = preds_out['preds'].groupby([preds_out['User_id'],preds_out['Coupon_id'],preds_out['Date_received']]).max()
 preds_out.to_csv('../data/preds_out.csv',index=False)
@@ -72,5 +77,4 @@ preds_out.to_csv('../data/preds_out.csv',index=False)
 preds_res.to_csv('../data/preds_res.csv',index=True)
 
 
-print(test_preds)
 
