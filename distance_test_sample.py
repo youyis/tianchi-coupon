58d2b341f981d75c605c93defc73005a40c07e24
nieyanping <nieyanping@jd.com>
2016-10-21 19:36:22 +0800

    modidfy distance sample

--- distance_test_sample.py
+++ distance_test_sample.py
@@ -2,8 +2,6 @@ import os
 import pandas as pd
 import numpy as np
 
-os.chdir("/data0/syy/coupon/tianchi-coupon")
-os.listdir(os.getcwd())
 offline = pd.read_csv('../data/base_sample.csv')
 distance_pos = offline['User_id'].groupby([offline['distance'],offline['label']]).count().reset_index()
 distance = []
@@ -12,9 +10,10 @@ test = pd.read_csv('../data/test_sample.csv')
 
 for i in xrange(test.shape[0]):
     dist = offline['distance'][i]
-    pos = distance_pos['User_id'][(distance_pos['distance'] == dist) & (distance_pos['label'] == 1)]
-    all = distance_pos['User_id'][(distance_pos['distance'] == dist)].sum()
-    distance.append(pos / all)
+    pos = distance_pos['User_id'][(distance_pos['distance'] == dist) & (distance_pos['label'] == 0)]
+    alldis = distance_pos['User_id'][(distance_pos['distance'] == dist)].sum()
+    dis_pro = pos / alldis
+    distance.append(dis_pro.values[0])
 offline['distance'] = distance
 
-offline.to_csv('../data/distance_sample_test.csv',index=False)
+test.to_csv('../data/distance_sample_test.csv',index=False)
--- distance_train_sample.py
+++ distance_train_sample.py
@@ -9,12 +9,12 @@ offline = pd.read_csv('../data/base_sample.csv')
 distance_pos = offline['User_id'].groupby([offline['distance'],offline['label']]).count().reset_index()
 distance = []
 
-
-for i in xrange(offline.shape[0]):ii
+for i in xrange(offline.shape[0]):
     dist = offline['distance'][i]
-    pos = distance_pos['User_id'][(distance_pos['distance'] == dist) & (distance_pos['label'] == 1)]
-    all = distance_pos['User_id'][(distance_pos['distance'] == dist)].sum()
-    distance.append(pos / all)
+    pos = distance_pos['User_id'][(distance_pos['distance'] == dist) & (distance_pos['label'] == 0)]
+    alldis = distance_pos['User_id'][(distance_pos['distance'] == dist)].sum()
+    dis_pro = pos / alldis
+    distance.append(dis_pro.values[0])
 offline['distance'] = distance
 
-offline.to_csv('../data/distance_sample.csv',index=False)
+offline.to_csv('../data/distance_sample_train.csv',index=False)
